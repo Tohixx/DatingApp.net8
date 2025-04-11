2827a405d09335956d1cc228882904a1ef30aabe
Tohixx <tohiix@gmail.com>
2025-04-11 20:08:13 +0200

    Routing in Angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { NgFor } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
+  imports: [NavComponent, HomeComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
