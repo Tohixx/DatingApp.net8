df07c8f99a853db257de10c79dc61c78f40661a3
Tohixx <tohiix@gmail.com>
2025-08-30 20:22:48 +0200

    Implement user filtering and pagination in member list

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
-import { Member } from '../../_models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
@@ -25,4 +25,11 @@ export class MemberListComponent implements OnInit {
     this.memberService.getMembers(this.pageNumber, this.pageSize)
   }
 
+
+  pageChanged(event: any){
+    if (this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
+  }
 }
