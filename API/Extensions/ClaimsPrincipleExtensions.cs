c392b93b5c06028af360f2204f139680d9676063
Tohixx <tohiix@gmail.com>
2025-09-10 20:28:09 +0200

    Enhance user activity logging and retrieval by adding user ID extraction, update member detail views with timeago and date formatting.

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -8,8 +8,15 @@ public static class ClaimsPrincipleExtensions
 {
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        var username = user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Cen not get username from token");
-        
+        var username = user.FindFirstValue(ClaimTypes.Name) ?? throw new Exception("Cen not get username from token");
+
         return username;
+    }
+    
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        var userId = int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Cen not get username from token"));
+        
+        return userId;
     }
 }
